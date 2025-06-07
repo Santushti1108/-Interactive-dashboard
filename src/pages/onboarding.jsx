00ed709605da4b522ce0176acccb285cde2327bd
Santushti Singh <singhsantushti111@gmail.com>
2025-06-07 11:19:00 +0530

    Initial commit: Multi-step onboarding form

--- src/pages/onboarding.jsx
+++ src/pages/onboarding.jsx
@@ -1,8 +1,8 @@
 import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
-import Step1 from '../components/Step1';
-import Step2 from '../components/Step2';
-import Step3 from '../components/Step3';
+import Step1 from '../components/step1';
+import Step2 from '../components/step2';
+import Step3 from '../components/step3';
 
 function Onboarding({ step, setStep, formData, setFormData }) {
   const navigate = useNavigate();
